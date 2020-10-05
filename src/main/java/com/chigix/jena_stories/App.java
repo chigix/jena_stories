dc44e5f7093f2af81ddfe0a3e3b07eb4d29bc535
Richard Lea <chigix@zoho.com>
2020-10-05 23:13:51 +0900

    feat: init workspace to play jena

---
+++ src/main/java/com/chigix/jena_stories/App.java
@@ -0,0 +1,40 @@
+package com.chigix.jena_stories;
+
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.VCARD;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+  static String personURI = "http://somewhere/JohnSmith";
+  static String fullName = "John Smith";
+  private static final Logger LOG = LoggerFactory.getLogger(App.class.getName());
+
+  /**
+   * A getting started graph initialized to confirm this maven project works for
+   * Jena.
+   *
+   * https://github.com/apache/jena/blob/master/jena-core/src-examples/jena/examples/rdf/Tutorial02.java
+   * https://jena.apache.org/tutorials/rdf_api.html#ch-Introduction
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    final Model model = ModelFactory.createDefaultModel();
+    final Resource johnSmith = model.createResource(personURI)
+        // A property from VCARD schema
+        .addProperty(VCARD.FN, fullName);
+    final String givenName = "John";
+    final String familyName = "Smith";
+    johnSmith.addProperty(VCARD.N, model.createResource()
+        .addProperty(VCARD.Given, givenName).addProperty(VCARD.Family, familyName));
+    LOG.info("The newly generated resource: {}", johnSmith);
+  }
+}
---
+++ src/test/java/com/chigix/jena_stories/AppTest.java
@@ -0,0 +1,20 @@
+package com.chigix.jena_stories;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
