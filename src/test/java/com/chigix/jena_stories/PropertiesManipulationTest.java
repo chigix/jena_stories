a7c7e215ead96e9bc9c016922e563c5febebce1d
Richard Lea <chigix@zoho.com>
2020-10-28 17:17:17 +0900

    test: complete special properties in property manipulation case

--- src/test/java/com/chigix/jena_stories/PropertiesManipulationTest.java
+++ src/test/java/com/chigix/jena_stories/PropertiesManipulationTest.java
@@ -3,6 +3,7 @@ package com.chigix.jena_stories;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
@@ -20,6 +21,7 @@ import org.apache.jena.ontology.OntProperty;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.vocabulary.XSD;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -34,6 +36,7 @@ public class PropertiesManipulationTest {
   private static final String NS = "http://chigix.com/else#";
   private String expectedOntologyPropertyResult;
   private String expectedConvertedFunctionalPropertyResult;
+  private String expectedSpecialProperties;
 
   @Before
   public void setUp() {
@@ -43,6 +46,9 @@ public class PropertiesManipulationTest {
           StandardCharsets.UTF_8);
       this.expectedConvertedFunctionalPropertyResult = IOUtils.toString(
           getClass().getClassLoader().getResourceAsStream("snapshot-functional-property-convert.owl"),
+          StandardCharsets.UTF_8);
+      this.expectedSpecialProperties = IOUtils.toString(
+          getClass().getClassLoader().getResourceAsStream("snapshot-special-properties.owl"),
           StandardCharsets.UTF_8);
     } catch (IOException e) {
       e.printStackTrace();
@@ -111,6 +117,33 @@ public class PropertiesManipulationTest {
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     m.write(baos);
     assertEquals(baos.toString(), expectedConvertedFunctionalPropertyResult);
+  }
+
+  @Test
+  public void testTransitiveProperty() {
+    OntModel m = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM_RULE_INF);
+    m.read(getClass().getClassLoader().getResourceAsStream("african-wildlife.owl"), NS);
+    ObjectProperty isPartOf = m.getObjectProperty(NS + "is_part_of");
+
+    // Special Properties are all ObjectProperties
+    assertTrue(isPartOf.isTransitiveProperty());
+  }
+
+  /**
+   * A simple example revised form A Semantic Web Primer
+   */
+  @Test
+  public void testSpecialProperties() {
+    OntModel m = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM_RULE_INF);
+    ObjectProperty p = m.createObjectProperty(NS + "hasSameGradeAs");
+    p.addRDFType(m.getResource("http://www.w3.org/2002/07/owl#TransitiveProperty"));
+    p.addRDFType(m.getResource("http://www.w3.org/2002/07/owl#SymmetricProperty"));
+    p.addDomain(m.getResource(NS + "student"));
+    p.addRange(m.getResource(NS + "student"));
+    m.createInverseFunctionalProperty(NS + "isTheSocialSecurityNumberfor");
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    m.write(baos);
+    assertThat(baos.toString(), Matchers.equalTo(expectedSpecialProperties));
   }
 
 }
