a8fc8d9bd37e9b40a9d97bc9e0f7ca1663887072
Richard Lea <chigix@zoho.com>
2020-10-28 23:28:16 +0900

    test: story case of building collection

---
+++ src/test/java/com/chigix/jena_stories/BooleanClassExpressionTest.java
@@ -0,0 +1,91 @@
+package com.chigix.jena_stories;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.RDFList;
+import org.apache.jena.rdf.model.RDFNode;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.xmlunit.builder.DiffBuilder;
+import org.xmlunit.builder.Input;
+import org.xmlunit.diff.Diff;
+
+public class BooleanClassExpressionTest {
+
+  private static final String NS = "http://chigix.com/else#";
+  private String expectedListConstruction;
+  private String expectedCollectionBuilding;
+
+  @Before
+  public void setUp() {
+    try {
+      expectedListConstruction = IOUtils.toString(
+          getClass().getClassLoader().getResourceAsStream("snapshot-list-construction.owl"),
+          StandardCharsets.UTF_8);
+      expectedCollectionBuilding = IOUtils.toString(
+          getClass().getClassLoader().getResourceAsStream("snapshot-collection-building.owl"),
+          StandardCharsets.UTF_8);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * https://jena.apache.org/documentation/ontology/#list-expressions
+   */
+  @Test
+  public void testListConstruction() {
+    OntModel m = ModelFactory.createOntologyModel();
+    OntClass c0 = m.createClass(NS + "c0");
+    OntClass c1 = m.createClass(NS + "c1");
+    OntClass c2 = m.createClass(NS + "c2");
+    RDFList classes = m.createList(new RDFNode[] { c0, c1, c2 });
+    assertTrue(classes.isResource());
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    m.write(baos);
+    assertThat(baos.toString(), Matchers.equalTo(expectedListConstruction));
+
+    assertThat(classes.size(), Matchers.is(3));
+
+    Iterator<RDFNode> i = classes.iterator();
+    assertThat(i.next().asResource().getURI(), Matchers.equalTo(NS + "c0"));
+    assertThat(i.next().asResource().getURI(), Matchers.equalTo(NS + "c1"));
+    assertThat(i.next().asResource().getURI(), Matchers.equalTo(NS + "c2"));
+  }
+
+  @Test
+  public void testCollectionBuilding() {
+    OntModel m = ModelFactory.createOntologyModel();
+    OntClass c0 = m.createClass(NS + "c0");
+    OntClass c1 = m.createClass(NS + "c1");
+    OntClass c2 = m.createClass(NS + "c2");
+    RDFList classes = m.createList(); // An empty list
+    // Build a list one element at time
+    // `cons` adds cell to the front of the list
+    classes.cons(c0);
+    classes.cons(c1);
+    classes.cons(c2);
+    assertTrue(classes.isResource());
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    m.write(baos);
+    Diff d = DiffBuilder.compare(Input.fromString(baos.toString()))
+        .withTest(Input.fromString(expectedCollectionBuilding))
+        .withNodeFilter(x -> !x.getNodeName().equals("rdf:Description")).build();
+    assertFalse(d.hasDifferences());
+  }
+
+}
